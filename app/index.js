7ab0b07cc9fd84a93c57bfb848414b3952f8b659
ChayaLipshitz <chl4165772@gmail.com>
2023-11-01 11:27:34 +0200

    commit for some change

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./server');
 require('./database');
 
-// Server is listening
+// Server is listening and running
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
   console.log('Environment:', process.env.NODE_ENV);
