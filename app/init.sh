a95d2decd237a0fec6175f449cb9c2e0d86c3934
ChayaLipshitz <chl4165772@gmail.com>
2023-11-01 11:13:40 +0200

    commit for jenkins

--- app/init.sh
+++ app/init.sh
@@ -2,6 +2,8 @@ docker build -t  note_task .
 
 docker volume create noteTask
 
+docker ps
+
 docker run --name chat-app-run --cpus "2.0" --memory "1g" -v noteTask-data:/data note_task
  -p 4000:4000 
 
