2e85c0b668515f9b4cbd714cdd83dbf5022df5be
ChayaLipshitz <chl4165772@gmail.com>
2023-11-01 11:18:47 +0200

    commit for jenkins2

--- app/init.sh
+++ app/init.sh
@@ -2,8 +2,7 @@ docker build -t  note_task .
 
 docker volume create noteTask
 
-docker ps
-
+docker images
 docker run --name chat-app-run --cpus "2.0" --memory "1g" -v noteTask-data:/data note_task
  -p 4000:4000 
 
